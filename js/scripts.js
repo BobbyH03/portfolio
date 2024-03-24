6ac09c738f41dce2c0948649275ff1a0f451b7c1
Robert Hoskins <137965495+BobbyH03@users.noreply.github.com>
2024-03-24 11:23:09 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,63 @@
+/*!
+* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+img = document.getElementById("img1");
+// Function to set image dimensions
+function enlargeImg1() {
+    img1.style.width = "80%";
+    img1.style.height = "auto";
+    img1.style.transition = "width 0.5s ease";
+}
+
+img = document.getElementById("img2");
+// Function to set image dimensions for 2nd image
+function enlargeImg2() {
+    img2.style.width = "80%";
+    img2.style.height = "auto";
+    img2.style.transition = "width 0.5s ease";
+}
+
+// Function to reset image dimensions
+function resetImg1() {
+    img1.style.width = "10%";
+    img1.style.height = "auto";
+    img1.style.transition = "width 0.5s ease";
+}
+// Function to reset image dimensions
+function resetImg2() {
+    img2.style.width = "10%";
+    img2.style.height = "auto";
+    img2.style.transition = "width 0.5s ease";
+}
